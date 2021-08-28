a26eb37f8d6210a51bef965990d90791666a02d7
sieugene <sieugene@mail.ru>
2021-08-28 14:49:44 +0300

    init react simple webpack

---
+++ react-simple/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Components/Button/index";
+
+const App = () => {
+  return (
+    <div>
+      Hello world!
+      <br />
+      <Button />
+    </div>
+  );
+};
+
+export default App;
---
+++ react-simple/src/Components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.scss";
+
+const Button = () => {
+  return <button>my button</button>;
+};
+
+export default Button;
